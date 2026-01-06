585385235a64962f1d246a50047b35a1a3bafe7b
Shatansh6 <pshatansh@gmail.com>
2026-01-06 23:10:59 +0530

    routing updated

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express, { json, urlencoded } from 'express';
 import cors from 'cors';
-import cookieParser from 'cookieParser';
+import cookieParser from 'cookie-Parser';
 
 const app = express();
 
@@ -11,4 +11,8 @@ app.use(cors({
 app.use(json({limit:'16kb'}))
 app.use(urlencoded({extended:true,limit:'16kb'}));
 app.use(cookieParser());
+
+import userRouter from './routes/user.route.js'
+app.use('/api/v1/users',userRouter);
+
 export {app};
